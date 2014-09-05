1b4766262f922c6cd1ce3184940038254a5a4a3b
Fabio Rehm <fgrehm@gmail.com>
2014-09-05 18:19:19 -0300

    Fix Wmi.ManagedComputer instantiation

--- configure-sql-port.ps1
+++ configure-sql-port.ps1
@@ -5,8 +5,8 @@
 
 # http://www.dbi-services.com/index.php/blog/entry/sql-server-2012-configuring-your-tcp-port-via-powershell
 # Set the port
-$wmi = new-object ($smo + 'Wmi.ManagedComputer')
 $smo = 'Microsoft.SqlServer.Management.Smo.'
+$wmi = new-object ($smo + 'Wmi.ManagedComputer')
 $uri = "ManagedComputer[@Name='WIN-2008R2-STD']/ ServerInstance[@Name='SQLEXPRESS']/ServerProtocol[@Name='Tcp']"
 $Tcp = $wmi.GetSmoObject($uri)
 $wmi.GetSmoObject($uri + "/IPAddress[@Name='IPAll']").IPAddressProperties[1].Value="1433"
